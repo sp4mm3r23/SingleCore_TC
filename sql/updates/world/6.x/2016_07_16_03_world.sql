a3683afe009e19732c42d1693c9c84b972b695d6
Ulduar <Ulduar@users.noreply.github.com>
2016-08-09 14:48:13 +0600

    Update 2016_07_16_03_world.sql

--- sql/updates/world/6.x/2016_07_16_03_world.sql
+++ sql/updates/world/6.x/2016_07_16_03_world.sql
@@ -2,3 +2,38 @@
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_subjugator_korul_darkness_calls';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (151159,'spell_subjugator_korul_darkness_calls');
+
+-- spell_dk_defile
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_defile';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(152280,'spell_dk_defile');
+
+-- spell_dk_remorseless_winter
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_remorseless_winter';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(115000,'spell_dk_remorseless_winter');
+
+-- spell_dk_desecrated_ground
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_desecrated_ground';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(118009,'spell_dk_desecrated_ground');
+
+-- spell_dk_sindragosa_breath
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_sindragosa_breath';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(155166,'spell_dk_sindragosa_breath');
+
+-- spell_dk_dark_transformation
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_dark_transformation';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(63560,'spell_dk_dark_transformation');
+
+-- spell_dk_shadow_infusion
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_shadow_infusion';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(91342,'spell_dk_shadow_infusion');
+
+-- spell_dk_dark_transformation_form
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_dark_transformation_form';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(63560,'spell_dk_dark_transformation_form');
