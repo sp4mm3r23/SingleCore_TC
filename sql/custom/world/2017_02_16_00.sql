05672cedee9bdbbad256bcf41e265003d4b52aa5
Apevia <Sonic2208@outlook.de>
2017-02-16 06:37:13 +0100

    Core/Spell: Fixed Desecrated Ground, Howling Blast, Gorefiend's Grasp, Conversion, Shroud of Purgatory, Purgatory

---
+++ sql/custom/world/2017_02_16_00.sql
@@ -0,0 +1,9 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (118009, 49184, 108199, 119975, 116888, 114556);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(118009,'spell_dk_desecrated_ground'),
+(49184,'spell_dk_howling_blast'),
+(108199,'spell_dk_gorefiends_grasp'),
+(119975,'spell_dk_conversion'),
+(116888,'spell_dk_purgatory'),
+(114556,'spell_dk_purgatory_absorb');
+
