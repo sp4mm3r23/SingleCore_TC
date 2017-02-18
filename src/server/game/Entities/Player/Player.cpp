allankrdec/TrinityCore-10-2024

Core/Instance: Fix GetInstanceSave sometimes checking the wrong map (#17856)

src/server/game/Entities/Player/Player.cpp
@@ -18590,7 +18590,7 @@ InstanceSave* Player::GetInstanceSave(uint32 mapid, bool raid)
     InstanceSave* pSave = pBind ? pBind->save : NULL;
     if (!pBind || !pBind->perm)
         if (Group* group = GetGroup())
-            if (InstanceGroupBind* groupBind = group->GetBoundInstance(this))
+            if (InstanceGroupBind* groupBind = group->GetBoundInstance(GetDifficulty(raid), mapid))
                 pSave = groupBind->save;
 
     return pSave;

sp4mm3r23/SingleCore_TC

(cherry picked from commit 9c504c8a8ec4db3b75ad35172827457905ede8ee)

src/server/game/Entities/Player/Player.cpp
@@ -19082,7 +19082,7 @@ InstanceSave* Player::GetInstanceSave(uint32 mapid)
     InstanceSave* pSave = pBind ? pBind->save : NULL;
     if (!pBind || !pBind->perm)
         if (Group* group = GetGroup())
-            if (InstanceGroupBind* groupBind = group->GetBoundInstance(this))
+            if (InstanceGroupBind* groupBind = group->GetBoundInstance(GetDifficultyID(mapEntry), mapid))
                 pSave = groupBind->save;
 
     return pSave;