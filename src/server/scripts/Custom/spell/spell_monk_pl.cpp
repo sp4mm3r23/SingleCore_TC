e6c3feb3250bcc65caa4858471e2f23764f02882
Apevia <Sonic2208@outlook.de>
2017-02-20 16:41:42 +0100

    Core/Spell: Fix Roll, add Chi Wave in Complete List

--- src/server/scripts/Custom/spell/spell_monk_pl.cpp
+++ src/server/scripts/Custom/spell/spell_monk_pl.cpp
@@ -25,7 +25,9 @@
 
 enum MonkSpells
 {
-    SPELL_MONK_CHI_WAVE_HEAL = 132463
+    SPELL_MONK_CHI_WAVE_HEAL                            = 132463,
+    SPELL_MONK_ROLL                                     = 109132,
+    SPELL_MONK_ROLL_TRIGGER                             = 107427
 };
 
 // 173545 - Chi Wave (healing bolt)
@@ -73,7 +75,59 @@ class spell_monk_chi_wave_healing_bolt: public SpellScriptLoader
         }
 };
 
+// 109132 - Roll
+// 7.x.x
+class spell_monk_roll : public SpellScriptLoader
+{
+    public:
+        spell_monk_roll() : SpellScriptLoader("spell_monk_roll") { }
+        class spell_monk_roll_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_monk_roll_SpellScript);
+            bool Validate(SpellInfo const* /*spell*/) override
+        {
+            if (!sSpellMgr->GetSpellInfo(SPELL_MONK_ROLL_TRIGGER))
+                return false;
+            return true;
+        }
+
+            bool Load() override
+            {
+                return GetCaster()->GetTypeId() == TYPEID_PLAYER;
+            }
+
+            void HandleBeforeCast()
+            {
+                Aura* aura = GetCaster()->AddAura(SPELL_MONK_ROLL_TRIGGER, GetCaster());
+                if (!aura)
+                    return;
+                AuraApplication* app = aura->GetApplicationOfTarget(GetCaster()->GetGUID());
+                if (!app)
+                    return;
+                app->ClientUpdate();
+            }
+
+            void HandleAfterCast()
+            {
+                Unit* caster = GetCaster();
+                caster->CastSpell(caster, SPELL_MONK_ROLL_TRIGGER, true);
+            }
+
+            void Register() override
+            {
+                BeforeCast += SpellCastFn(spell_monk_roll_SpellScript::HandleBeforeCast);
+                AfterCast += SpellCastFn(spell_monk_roll_SpellScript::HandleAfterCast);
+            }
+        };
+
+        SpellScript* GetSpellScript() const override
+        {
+            return new spell_monk_roll_SpellScript();
+        }
+};
+
 void AddSC_monk_spell_scripts_pl()
 {
     new spell_monk_chi_wave_healing_bolt();
+    new spell_monk_roll();
 }
