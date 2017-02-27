2a49af568314c2c1d9854db5056f1cd70644af09
zgn <zgn88@mail.ru>
2017-02-27 22:24:42 +0300

    Core/Script: base enum Eye Of Azshara

--- src/server/scripts/Custom/zone/BrokenIsles/Dungeons/EyeOfAzshara/eye_of_azshara.h
+++ src/server/scripts/Custom/zone/BrokenIsles/Dungeons/EyeOfAzshara/eye_of_azshara.h
@@ -14,3 +14,37 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+#ifndef EYE_OF_AZSHARA_H_
+#define EYE_OF_AZSHARA_H_
+
+#define EoAScriptName "instance_eye_of_azshara"
+#define DataHeader    "EOA"
+
+uint32 const EncounterCount = 5;
+
+enum EOADataTypes
+{
+    DATA_SERPENTRIX       = 0,
+    DATA_WARLORD_PARJESH  = 1,
+    DATA_LADY_HATECOIL    = 2,
+    DATA_KING_DEEPBEARD   = 3,
+    DATA_WRATH_OF_AZSHARA = 4
+};
+
+enum EOACreatureIds
+{
+    NPC_SERPENTRIX       = 91808,
+    NPC_WARLORD_PARJESH  = 91784,
+    NPC_LADY_HATECOIL    = 91789,
+    NPC_KING_DEEPBEARD   = 91797,
+    NPC_WRATH_OF_AZSHARA = 96028
+};
+
+template<class AI>
+AI* GetEyeOfAzsharaAI(Creature* creature)
+{
+    return GetInstanceAI<AI>(creature, EoAScriptName);
+}
+
+#endif // EYE_OF_AZSHARA_H_
