03227442a32369d591232c416a3a1f16c27b5b24
zgn <zgn88@mail.ru>
2017-03-09 23:35:35 +0300

    DB/GrimBatol: update Broadcast Text

--- sql/updates/world/master/2017_03_09_00_pl.sql
+++ sql/updates/world/master/2017_03_09_00_pl.sql
@@ -2,13 +2,13 @@ UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'boss_drahga_shadow
 
 DELETE FROM `creature_text` WHERE `entry` = 40319;
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`, `BroadcastTextID`) VALUES
-('40319','0','0','I will burn you from the inside out!','14','0','0','0','0','18610','VO_GB_Drahga_Engage','0'),
-('40319','1','0','INCINERATE THEM, MINIONS!','14','0','0','0','0','18616','VO_GB_Drahga_Sum_Spirits1','0'),
-('40319','1','1','BY FIRE BE... BURNED!','14','0','0','0','0','18619','VO_GB_Drahga_Sum_Spirits2','0'),
-('40319','2','0','Dragon, you will do as I command! Catch me!','14','0','0','0','0','18621','VO_GB_Drahga_Sum_Valiona','0'),
-('40319','3','0','You should have come better prepared!','14','0','0','0','0','18612','VO_GB_Drahga_Slay01','0'),
-('40319','3','1','An easy kill!','14','0','0','0','0','18611','VO_GB_Drahga_Slay02','0'),
-('40319','4','0','Valiona, finish them! Avenge me!','14','0','0','0','0','18614','VO_GB_Drahga_Death','0');
+('40319','0','0','I will burn you from the inside out!','14','0','0','0','0','18610','VO_GB_Drahga_Engage','47760'),
+('40319','1','0','INCINERATE THEM, MINIONS!','14','0','0','0','0','18616','VO_GB_Drahga_Sum_Spirits1','47765'),
+('40319','1','1','BY FIRE BE... BURNED!','14','0','0','0','0','18619','VO_GB_Drahga_Sum_Spirits2','47766'),
+('40319','2','0','Dragon, you will do as I command! Catch me!','14','0','0','0','0','18621','VO_GB_Drahga_Sum_Valiona','47767'),
+('40319','3','0','You should have come better prepared!','14','0','0','0','0','18612','VO_GB_Drahga_Slay01','47762'),
+('40319','3','1','An easy kill!','14','0','0','0','0','18611','VO_GB_Drahga_Slay02','47761'),
+('40319','4','0','Valiona, finish them! Avenge me!','14','0','0','0','0','18614','VO_GB_Drahga_Death','47764');
 
 DELETE FROM `creature_loot_template` WHERE `Entry` = 40319;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
@@ -34,9 +34,9 @@ UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'boss_erudax' WHERE
 
 DELETE FROM `creature_text` WHERE `entry` = 40484;
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`, `BroadcastTextID`) VALUES
-('40484','0','0','The darkest days are still ahead!','14','0','0','0','0','18638','VO_GB_Erudax_Engage','0'),
-('40484','1','0','Flakh ghet! The shadows hunger cannot be sated! ','14','0','0','0','0','18644','VO_GB_Erudax_Shad_Gale','0'),
-('40484','2','0','Come, suffering... Enter, chaos!','14','0','0','0','0','18646','VO_GB_Erudax_Guardian','0'),
+('40484','0','0','The darkest days are still ahead!','14','0','0','0','0','18638','VO_GB_Erudax_Engage','47770'),
+('40484','1','0','Flakh ghet! The shadows hunger cannot be sated!','14','0','0','0','0','18644','VO_GB_Erudax_Shad_Gale','0'),
+('40484','2','0','Come, suffering... Enter, chaos!','14','0','0','0','0','18646','VO_GB_Erudax_Guardian','40612'),
 ('40484','3','0','Flesh for the offering!','14','0','0','0','0','18640','VO_GB_Erudax_Slay02','0'),
 ('40484','3','1','Erudax cackles maniacally.','16','0','0','0','0','18639','VO_GB_Erudax_Slay01','0'),
 ('40484','4','0','Ywaq maq oou; ywaq maq ssaggh. Yawq ma shg fhn.','14','0','0','0','0','18641','VO_GB_Erudax_Death','0');
@@ -68,13 +68,13 @@ UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'boss_forgemaster_t
 
 DELETE FROM `creature_text` WHERE `entry` = 40177;
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`, `BroadcastTextID`) VALUES
-('40177','0','0','NO! Throngus get whipped again if he no finish!','14','0','0','0','0','18943','VO_GB_Throngus_Engage','0'),
-('40177','1','0','You not get through defenses!','14','0','0','0','0','18947','VO_GB_Throngus_Shield','0'),
-('40177','2','0','Throngus SLICE you up!','14','0','0','0','0','18948','VO_GB_Throngus_Sword','0'),
+('40177','0','0','NO! Throngus get whipped again if he no finish!','14','0','0','0','0','18943','VO_GB_Throngus_Engage','47777'),
+('40177','1','0','You not get through defenses!','14','0','0','0','0','18947','VO_GB_Throngus_Shield','47781'),
+('40177','2','0','Throngus SLICE you up!','14','0','0','0','0','18948','VO_GB_Throngus_Sword','47783'),
 ('40177','3','0','Oh, this is gonna HURT!','14','0','0','0','0','18949','VO_GB_Throngus_Mace','0'),
-('40177','4','0','You break easy!','14','0','0','0','0','18945','VO_GB_Throngus_Slay01','0'),
-('40177','4','1','Throngus use your corpse on body. Somewhere...','14','0','0','0','0','18944','VO_GB_Throngus_Slay02','0'),
-('40177','5','0','Death... Good choice. Not best choice maybe, but better than fail and live.','14','0','0','0','0','18946','VO_GB_Throngus_Death','0');
+('40177','4','0','You break easy!','14','0','0','0','0','18945','VO_GB_Throngus_Slay01','47779'),
+('40177','4','1','Throngus use your corpse on body. Somewhere...','14','0','0','0','0','18944','VO_GB_Throngus_Slay02','47778'),
+('40177','5','0','Death... Good choice. Not best choice maybe, but better than fail and live.','14','0','0','0','0','18946','VO_GB_Throngus_Death','47780');
 
 DELETE FROM `creature_loot_template` WHERE `Entry` = 40177;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
@@ -99,12 +99,12 @@ DELETE FROM `creature_text` WHERE `entry` = 39625;
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`, `BroadcastTextID`) VALUES
 ('39625','0','0','A million more await my orders. What chance you do have?','14','0','0','0','0','18530','umbriss - SAY_AGGRO','0'),
 ('39625','1','0','Cover the rear! Alexstraza\'s brood decimate our rank!','14','0','0','0','0','18537','umbriss - SAY_BOMBING_1','0'),
-('39625','2','0','Reinforce the front! We are being invaded!','14','0','0','0','0','18536','umbriss - SAY_BOMBING_2','0'),
-('39625','3','0','Attack you cowardly vermin!','14','0','0','0','0','18535','umbriss - SAY_SUMMON','0'),
+('39625','2','0','Reinforce the front! We are being invaded!','14','0','0','0','0','18536','umbriss - SAY_BOMBING_2','40640'),
+('39625','3','0','Attack you cowardly vermin!','14','0','0','0','0','18535','umbriss - SAY_SUMMON','47799'),
 ('39625','3','1','Cover the rear! Alexstrazas brood decimate our rank!','14','0','0','0','0','18537','VO_GB_Umbriss_Bomb2','0'),
-('39625','4','0','Messy...','14','0','0','0','0','18532','umbriss - SAY_KILL_1','0'),
-('39625','4','1','Vermin, your dinner awaits!','14','0','0','0','0','18531','VO_GB_Umbriss_Slay02','0'),
-('39625','5','0','Vermin, your dinner awaits!','14','0','0','0','0','18531','umbriss - SAY_KILL_2','0'),
+('39625','4','0','Messy...','14','0','0','0','0','18532','umbriss - SAY_KILL_1','47786'),
+('39625','4','1','Vermin, your dinner awaits!','14','0','0','0','0','18531','VO_GB_Umbriss_Slay02','47785'),
+('39625','5','0','Vermin, your dinner awaits!','14','0','0','0','0','18531','umbriss - SAY_KILL_2','47785'),
 ('39625','6','0','General Umbriss sets his eyes on $N and begins to cast blitz.','41','0','0','0','0','18533','umbriss - EMOTE_BLITZ','0'),
 ('39625','7','0','General Umbriss begins to cast ground siege.','41','0','0','0','0','18534','umbriss - EMOTE_GROUND_SIEGE','0');
 
