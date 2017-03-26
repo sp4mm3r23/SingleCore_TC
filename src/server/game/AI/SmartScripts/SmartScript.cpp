52c97b039cac54c1eaa16355e6b2a2741f7d0495
ConanHUN <conan_513@hotmail.com>
2017-03-26 17:34:20 +0200

    Double case SMART_TARGET_VEHICLE_ACCESSORY

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -2933,14 +2933,6 @@ ObjectList* SmartScript::GetTargets(SmartScriptHolder const& e, Unit* invoker /*
             }
             break;
         }
-        case SMART_TARGET_VEHICLE_ACCESSORY:
-        {
-            if (me && me->IsVehicle())
-            {
-                if (Unit* target = me->GetVehicleKit()->GetPassenger(e.target.vehicle.seat))
-                    l->push_back(target);
-            }
-        }
         case SMART_TARGET_POSITION:
         case SMART_TARGET_NONE:
         default:
